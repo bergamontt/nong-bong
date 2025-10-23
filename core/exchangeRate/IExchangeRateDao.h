5d0335764af4a752466e91a47f7dcb9bab273c1a
bergamontt <dariamumbo@gmail.com>
2025-10-24 00:58:42 +0300

    Fixed file name typo

--- core/exchangeRate/IExchangeRateDao.h
+++ core/exchangeRate/IExchangeRateDao.h
@@ -2,23 +2,23 @@
 #include <vector>
 #include "ExchangeRate.h"
 
-class IExchangeRate
+class IExchangeRateDao
 {
 public:
     ExchangeRate getById(int id) const;
     std::vector<ExchangeRate> getAll() const;
-    virtual ~IExchangeRate() = default;
+    virtual ~IExchangeRateDao() = default;
 private:
     virtual ExchangeRate doGetById(int id) const = 0;
     virtual std::vector<ExchangeRate> doGetAll() const = 0;
 };
 
-inline ExchangeRate IExchangeRate::getById(const int id) const
+inline ExchangeRate IExchangeRateDao::getById(const int id) const
 {
     return doGetById(id);
 }
 
-inline std::vector<ExchangeRate> IExchangeRate::getAll() const
+inline std::vector<ExchangeRate> IExchangeRateDao::getAll() const
 {
     return doGetAll();
 }
