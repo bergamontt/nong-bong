005fe77e4094cc5752aec4d53291f2b7ee30a2b4
bergamontt <dariamumbo@gmail.com>
2025-10-23 10:40:53 +0300

    Added type conversion to the other half of the entities

--- core/domain/BankTransaction.h
+++ core/domain/BankTransaction.h
@@ -2,6 +2,7 @@
 #include <optional>
 #include <string>
 #include <ctime>
+#include <soci/soci.h>
 
 class BankTransaction
 {
@@ -13,6 +14,40 @@ public:
     std::optional<int> toCardId;
     int amount;
     std::string currencyCode;
-    std::optional<std::string> description;
+    std::string description;
+    std::optional<std::string> comment;
     std::string status;
+};
+
+template <>
+struct soci::type_conversion<BankTransaction>
+{
+    typedef values base_type;
+    static void from_base(values const& v, indicator, BankTransaction& c)
+    {
+        c.id = v.get<int>("id");
+        c.createdAt = v.get<std::tm>("created_at");
+        c.type = v.get<std::string>("type");
+        c.fromCardId = v.get<int>("from_card_id");
+        c.toCardId = v.get<int>("to_card_id");
+        c.amount = v.get<int>("amount");
+        c.currencyCode = v.get<std::string>("currency_code");
+        c.description = v.get<std::string>("description");
+        c.comment = v.get<std::string>("comment");
+        c.status = v.get<std::string>("status");
+    }
+
+    static void to_base(const BankTransaction& c, values& v, indicator& ind)
+    {
+        v.set("id", c.id);
+        v.set("created_at", c.createdAt);
+        v.set("type", c.type);
+        v.set("from_card_id", c.fromCardId, c.fromCardId.has_value() ? i_ok : i_null);
+        v.set("to_card_id", c.toCardId, c.toCardId.has_value() ? i_ok : i_null);
+        v.set("amount", c.amount);
+        v.set("currency_code", c.currencyCode);
+        v.set("description", c.description);
+        v.set("comment", c.comment, c.comment.has_value() ? i_ok : i_null);
+        v.set("status", c.status);
+    }
 };
--- core/domain/ScheduledTransfer.h
+++ core/domain/ScheduledTransfer.h
@@ -2,6 +2,7 @@
 #include <optional>
 #include <string>
 #include <ctime>
+#include <soci/soci.h>
 
 class ScheduledTransfer
 {
@@ -15,4 +16,38 @@ public:
     std::optional<std::tm> nextTun;
     int active;
     std::string description;
+    std::optional<std::string> comment;
+};
+
+template <>
+struct soci::type_conversion<ScheduledTransfer>
+{
+    typedef values base_type;
+    static void from_base(values const& v, indicator, ScheduledTransfer& s)
+    {
+        s.id = v.get<int>("id");
+        s.fromCardId = v.get<int>("from_card_id");
+        s.toCardId = v.get<int>("to_card_id");
+        s.amount = v.get<int>("amount");
+        s.currencyCode = v.get<std::string>("currency_code");
+        s.frequency = v.get<std::string>("frequency");
+        s.nextTun = v.get<std::tm>("next_tun");
+        s.active = v.get<int>("active");
+        s.description = v.get<std::string>("description");
+        s.comment = v.get<std::string>("comment");
+    }
+
+    static void to_base(const ScheduledTransfer& s, values& v, indicator& ind)
+    {
+        v.set("id", s.id);
+        v.set("from_card_id", s.fromCardId);
+        v.set("to_card_id", s.toCardId);
+        v.set("amount", s.amount);
+        v.set("currency_code", s.currencyCode);
+        v.set("frequency", s.frequency);
+        v.set("next_tun", s.nextTun, s.nextTun.has_value() ? i_ok : i_null);
+        v.set("active", s.active);
+        v.set("description", s.description);
+        v.set("comment", s.comment, s.comment.has_value() ? i_ok : i_null);
+    }
 };
